f4ba13c085d7e27c0981ab149aad336d9b3988a6
张鹏程 <350803393@qq.com>
2019-07-24 13:30:23 +0800

    update

--- Beekey/settings.py
+++ Beekey/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_CREDENTIALS = True # 允许携带cookie
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'Beekey.urls'
 
